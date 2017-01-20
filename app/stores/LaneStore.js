c38d51e70224f10cf37ca52546d449706ded4b8a
Tendai Mutunhire <tmutunhire@gmail.com>
2017-01-20 23:00:38 +0200

    add lanes

---
+++ app/stores/LaneStore.js
@@ -0,0 +1,17 @@
+import LaneActions from '../actions/LaneActions';
+
+export default class LaneStore {
+  constructor() {
+    this.bindActions(LaneActions);
+
+    this.lanes = [];
+  }
+
+  create(lane) {
+    lane.notes = lane.notes || [];
+
+    this.setState({
+      lanes: this.lanes.concat(lane)
+    });
+  }
+}
