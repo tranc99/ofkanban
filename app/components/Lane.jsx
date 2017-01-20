3b600ce64824abf8e6d7dbc7a0f2c62b6f68418b
Tendai Mutunhire <tmutunhire@gmail.com>
2017-01-21 00:56:18 +0200

    Style the lanes

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -1,49 +1,35 @@
+/* @flow */
 import React from 'react';
-import uuid from 'uuid';
 import connect from '../libs/connect';
 import NoteActions from '../actions/NoteActions';
 import LaneActions from '../actions/LaneActions';
 import Notes from './Notes';
+import LaneHeader from './LaneHeader';
 
-const Lane = ({
-  lane, notes, LaneActions, NoteActions, ...props
-}) => {
+const Lane = ({ lane, notes, LaneActions, NoteActions, ...props}) => {
   const editNote = (id, task) => {
     NoteActions.update({id, task, editing: false});
   };
-  const addNote = e => {
-    e.stopPropagation();
 
-    const noteId = uuid.v4();
-
-    NoteActions.create({
-      id: noteId,
-      task: 'New task'
-    });
-
-    LaneActions.attachsToLane({
-
-    })
-  };
   const deleteNote = (noteId, e) => {
     e.stopPropagation();
 
+    LaneActions.detachFromLane({
+      laneId: lane.id,
+      noteId
+    });
     NoteActions.delete(noteId);
   };
+
   const activateNoteEdit = id => {
     NoteActions.update({id, editing: true});
   };
 
   return (
     <div {...props}>
-      <div className="lane-header">
-        <div className="lane-add-note">
-          <button onClick={addNote}>+</button>
-        </div>
-        <div className="lane-name">{lane.name}</div>
-      </div>
+      <LaneHeader lane={lane} />
       <Notes
-        notes={notes}
+        notes={selectNotesByIds(notes, lane.notes)}
         onNoteClick={activateNoteEdit}
         onEdit={editNote}
         onDelete={deleteNote} />
@@ -51,10 +37,18 @@ const Lane = ({
   );
 };
 
+function selectNotesByIds(allNotes, noteIds=[]) {
+  return noteIds.reduce((notes, id) =>
+    notes.concat(allNotes.filter(note => note.id === id))
+    , []
+  );
+}
+
 export default connect(
   ({notes}) => ({
     notes
   }), {
-    NoteActions
+    NoteActions,
+    LaneActions
   }
 )(Lane)
