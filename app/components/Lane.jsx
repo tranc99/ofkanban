ce4db461cb3a68a2b94296881dbdcff68f370636
Tendai Mutunhire <tmutunhire@gmail.com>
2017-01-21 03:57:39 +0200

    add tasks everywhere

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -1,12 +1,17 @@
 /* @flow */
 import React from 'react';
+
+import {compose} from 'redux';
+import {DropTarget} from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
+
 import connect from '../libs/connect';
 import NoteActions from '../actions/NoteActions';
 import LaneActions from '../actions/LaneActions';
 import Notes from './Notes';
 import LaneHeader from './LaneHeader';
 
-const Lane = ({ lane, notes, LaneActions, NoteActions, ...props}) => {
+const Lane = ({ connectDropTarget, lane, notes, LaneActions, NoteActions, ...props}) => {
   const editNote = (id, task) => {
     NoteActions.update({id, task, editing: false});
   };
@@ -25,7 +30,7 @@ const Lane = ({ lane, notes, LaneActions, NoteActions, ...props}) => {
     NoteActions.update({id, editing: true});
   };
 
-  return (
+  return connectDropTarget(
     <div {...props}>
       <LaneHeader lane={lane} />
       <Notes
@@ -44,11 +49,35 @@ function selectNotesByIds(allNotes, noteIds=[]) {
   );
 }
 
-export default connect(
-  ({notes}) => ({
+const noteTarget = {
+  hover(targetProps, monitor) {
+    const sourceProps = monitor.getItem();
+    const sourceId = sourceProps.id;
+
+    // If the target lane doesn't have notes,
+    // attach the note to it.
+    //
+    // `attachToLane` performs necessarly
+    // cleanup by default and it guarantees
+    // a note can belong only to a single lane
+    // at a time.
+    if(!targetProps.lane.notes.length) {
+      LaneActions.attachToLane({
+        laneId: targetProps.lane.id,
+        noteId: sourceId
+      });
+    }
+  }
+};
+
+export default compose(
+  DropTarget(ItemTypes.NOTE, noteTarget, connect => ({
+    connectDropTarget: connect.dropTarget()
+  })),
+  connect(({notes}) => ({
     notes
   }), {
     NoteActions,
     LaneActions
-  }
+  })
 )(Lane)
